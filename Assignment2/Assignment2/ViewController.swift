3e0970c9aa4da3acfb217ba01de11f74606204ab
yutinglia <yutinglia@gmail.com>
2022-01-02 12:43:20 -0800

    init ios project

---
+++ Assignment2/Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by YuTing Lai on 2/1/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
