3b505694356fa736cb9e07e717aa8086d5868a9d
yutinglia <yutinglia@gmail.com>
2022-01-06 14:16:53 -0800

    update sever access token api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const Account = require('../model/Account')
+const Account = require('../model/Account');
+var jwt = require('jsonwebtoken');
+var { v4: uuidv4 } = require('uuid');
+var config = require("../config");
 
 // get access token
 router.post('/', async function (req, res, next) {
@@ -11,11 +14,13 @@ router.post('/', async function (req, res, next) {
       // login fail
       res.json({ account: "0", email: "0", phone: "0", token: "0" });
     } else {
+      const uuid = uuidv4();
+      const token = jwt.sign({ ac, uuid }, config.JWT_SECRET_KEY, { expiresIn: config.JWT_EXPIRES_TIME });
       res.json({
         account: ac,
         email: account.email,
         phone: account.phone,
-        token: "i am token"
+        token: token
       });
     }
   } catch (err) {
