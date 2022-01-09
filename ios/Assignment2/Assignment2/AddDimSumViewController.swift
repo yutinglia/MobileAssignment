54367fec1fc273988bceff7d7ccb7003661ac5f2
yutinglia <yutinglia@gmail.com>
2022-01-10 06:54:35 +0800

    debug and update add dim sum funtion

---
+++ ios/Assignment2/Assignment2/AddDimSumViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddDimSumViewController.swift
+//  Assignment2
+//
+//  Created by YuTing Lai on 10/1/2022.
+//
+
+import UIKit
+
+class AddDimSumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
