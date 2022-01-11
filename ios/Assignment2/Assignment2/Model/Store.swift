0a85d4514b3cb7381d53526254233dd32460daf9
yutinglia <yutinglia@gmail.com>
2022-01-12 05:08:38 +0800

    .

---
+++ ios/Assignment2/Assignment2/Model/Store.swift
@@ -0,0 +1,8 @@
+//
+//  Store.swift
+//  Assignment2
+//
+//  Created by YuTing Lai on 12/1/2022.
+//
+
+import Foundation
---
+++ ios/Assignment2/Assignment2/ResetPasswordViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResetPasswordViewController.swift
+//  Assignment2
+//
+//  Created by YuTing Lai on 11/1/2022.
+//
+
+import UIKit
+
+class ResetPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
